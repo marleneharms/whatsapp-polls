90505e4ce370328d7f4a5b058c38abaab35b789c
Jorge Sanchez <sandix390@gmail.com>
2022-10-31 01:02:04 -0600

    added poll CRUD

--- backend/controllers/poll.controller.js
+++ backend/controllers/poll.controller.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const pollServices = require('../services/poll.services');
 
 const dummyPolls = [
     {
@@ -41,7 +42,51 @@ const dummyPolls = [
 const dummyPhones = ['523333549944', '523333864523', '523317072870', '523315997504']
 
 exports.getAllPolls = (req, res, next) => {
-    return res.status(200).json(dummyPolls);
+    pollServices.index((err, polls) => {
+        if (err){
+            return next(err);
+        }
+        return res.status(200).send({
+            message: 'Polls Found',
+            data: polls,
+        });
+    });
+};
+
+exports.getPollById = (req, res, next) => {
+    pollServices.show(req.params.id, (err, results) => {
+        if (err){
+            return next(err);
+        }
+        return res.status(200).send({
+            message: 'Poll Found',
+            data: results,
+        });
+    });
+};
+
+exports.createPoll = (req, res, next) => {
+    pollServices.create(req.body, (err, results) => {
+        if (err){
+            return next(err);
+        }
+        return res.status(201).send({
+            message: 'Poll Created',
+            data: results,
+        });
+    });
+};
+
+exports.deletePoll = (req, res, next) => {
+    pollServices.remove(req.params.id, (err, results) => {
+        if (err){
+            return next(err);
+        }
+        return res.status(202).send({
+            message: 'Deleted Successfully',
+            data: results,
+        });
+    });
 };
 
 exports.testWhats = (req, res, next) => {
