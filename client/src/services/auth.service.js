b9bb7a482ff0cc9dfb3ecf5ca62287b9d5f71ed2
Jorge Sanchez <sandix390@gmail.com>
2022-10-31 13:38:03 -0600

    added config for deployment

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/users";
+const API_URL = `${process.env.REACT_APP_API_URL}/users`;
 
 const register = (username, email, password) => {
     return axios
